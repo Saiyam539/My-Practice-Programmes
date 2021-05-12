0f9950ba2ecabe8cf79e95f0331f69b67ee5020c
Saiyam Arora <77787626+Saiyam539@users.noreply.github.com>
2021-05-12 14:43:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# This program will tell whether a number is odd 
+# or even with the help of python.
+
+def function():
+    num = int(input('Enter the number here:- '))
+
+    if num%2:
+        print(f'{num} is odd number.')
+    else:
+        print(f'{num} is a even number.')
+    
+function()
